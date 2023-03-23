66fc975364b1f5056d476e59d05edcc3e9e62f5a
Sylwia <stefansylwia@interia.pl>
2023-03-23 12:29:15 +0100

    c

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -2,10 +2,8 @@ import axios from 'axios';
 
 const request = axios.create({
     baseURL: 'https://mern-task-app-api-8ty5.onrender.com',
-    validateStatus: () => true,
-    headers: {'Access-Control-Allow-Origin': '*'}
+    // validateStatus: () => true,
+    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
 })
-
-// , 'Content-Type': 'application/json'
 
 export default request;
