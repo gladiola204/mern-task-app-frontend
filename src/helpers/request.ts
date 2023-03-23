d084016044c46e2d23f47858ba77ac8c76976be1
Sylwia <stefansylwia@interia.pl>
2023-03-23 12:19:23 +0100

    preflight Cors

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const request = axios.create({
     baseURL: 'https://mern-task-app-api-8ty5.onrender.com',
-    validateStatus: () => false,
-    headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
+    validateStatus: () => true,
+    headers: {'Access-Control-Allow-Origin': '*'}
 })
+
+// , 'Content-Type': 'application/json'
 
 export default request;
